5592f0b87c16be74857ac4fbec0e0a9153811b8b
Nachop51 <nachoperalta0@gmail.com>
2023-08-28 15:59:06 -0300

    hot fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float]:
+async def async_generator() -> Generator[float, None, None]:
     """ Async generator """
     for _ in range(10):
         await asyncio.sleep(1)
