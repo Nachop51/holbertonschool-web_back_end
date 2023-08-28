88f257beb629702de5e08d5e09c0aeb45bf6ec36
Nachop51 <nachoperalta0@gmail.com>
2023-08-28 15:21:25 -0300

    Async comprehension

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Async comprehension - comprehensions """
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """ Async comprehensions """
+    return [number async for number in async_generator()]
---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Async comprehension - measure runtime """
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ Measures the total execution time for async_comprehension four times """
+    start = asyncio.get_event_loop().time()
+    tasks = [asyncio.create_task(async_comprehension()) for _ in range(4)]
+    await asyncio.gather(*tasks)
+    end = asyncio.get_event_loop().time()
+    return end - start
+
+
+async def main():
+    return await(measure_runtime())
+
+print(
+    asyncio.run(main())
+)
